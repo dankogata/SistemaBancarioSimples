309aa4a01916a76e2d01c876ace68dd411d63d86
Daniel Ogata <47324513+dankogata@users.noreply.github.com>
2023-07-30 14:54:56 -0300

    Criação do sistema bancário

---
+++ atividade1.py
@@ -0,0 +1,44 @@
+LIMITE_SAQUE_DIA = 3
+LIMITE_SAQUE_VALOR = 500
+i=0
+contador =0
+extrato =0
+menu ="""
+[s] saque
+[d] Depositar
+[e] Extrato
+[q] Sair
+"""
+
+while True:
+    opcao = input(menu)
+    if opcao == "d":
+        deposito= float(input("Qual o valor do deposito: "))   
+        if deposito>0:
+            extrato += deposito
+            contador+=1
+        else:
+            print ("valor incorreto")
+           
+    elif opcao == "e":
+        print("\n=================EXTRATO==================")
+        print(f"Foram feitas {contador} movimentações \n")
+        print(f"O saldo da conta é de: R$ {extrato} \n", )
+        print("Sistema finalizado com sucesso")
+        print("\n==========================================")
+        break
+    
+    elif opcao == "s":
+        saque= float(input("Qual o valor do saque: "))
+        if saque>LIMITE_SAQUE_VALOR:
+            print("Valor acima do permitido")
+        elif i<3:
+            extrato -= saque
+            i+=1
+            contador+=1
+        else:
+            print("Limite de saques diários permitidos")   
+        
+    elif opcao == "q":
+        print("Sistema finalizado com sucesso")
+        break
